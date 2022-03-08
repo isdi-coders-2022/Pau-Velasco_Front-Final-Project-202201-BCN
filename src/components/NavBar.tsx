d1d45bc7cdcca29f2598db840102f07bc8aeee8e
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-08 13:57:54 +0100

    add nav-bar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,23 +4,43 @@ import {
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const ListItem = styled.li`
+  list-style: none;
+`;
+
+const ListContainer = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+`;
+
+const NavigationContainer = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+`;
 
 const NavBar = (): JSX.Element => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <FontAwesomeIcon icon={faUsers} />
-          </li>
-          <li>
+      <NavigationContainer>
+        <ListContainer>
+          <ListItem>
             <FontAwesomeIcon icon={faArrowRightToBracket} />
-          </li>
-          <li>
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon icon={faUsers} />
+          </ListItem>
+          <ListItem>
             <FontAwesomeIcon icon={faUserPlus} />
-          </li>
-        </ul>
-      </nav>
+          </ListItem>
+        </ListContainer>
+      </NavigationContainer>
     </>
   );
 };
