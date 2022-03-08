41ddc86a6569f900ac1f37309c8f961c1fdb6bcd
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-08 17:18:09 +0100

    add routes on index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <Routes>
+      <Route path="/" element={<NavBar />} />
+    </Routes>
+  );
 }
 
 export default App;
