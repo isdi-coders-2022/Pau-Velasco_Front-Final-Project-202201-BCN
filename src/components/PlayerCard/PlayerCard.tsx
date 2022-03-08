5322e3c596c9b8eeb77b3187b47afff573f998ee
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-08 18:46:17 +0100

    add styles to header card

--- src/components/PlayerCard/PlayerCard.tsx
+++ src/components/PlayerCard/PlayerCard.tsx
@@ -1,17 +1,46 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  background-color: grey;
+  padding: 10px;
+  display: flex;
+  border-radius: 3px;
+
+  & img {
+    width: 50px;
+    height: 60px;
+    object-fit: cover;
+    border-radius: 5px;
+    filter: drop-shadow(4px 0 4px #0000003f);
+  }
+
+  & p {
+    margin: 0;
+    font-size: 18px;
+    font-weight: 900;
+  }
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`;
+
 const PlayerCard = (): JSX.Element => {
   return (
     <>
       <section>
-        <div>
+        <HeaderContainer>
           <img
             src="https://img.uefa.com/imgml/TP/players/1/2022/324x324/63706.jpg?imwidth=36"
             alt=""
           />
           <div>
-            <h2>RONALDO</h2>
-            <h3>DORSAL: 7</h3>
+            <p>RONALDO</p>
+            <p>DORSAL: 7</p>
           </div>
-        </div>
+        </HeaderContainer>
       </section>
     </>
   );
