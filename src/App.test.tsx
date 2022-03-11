23e98f029f125a4488d0554f31557770467a1c01
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-11 12:02:55 +0100

    added providers on app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import store from "./redux/store";
 
 describe("Given an App page", () => {
   describe("When it's rendered", () => {
     test("Then it should render the NavBar", () => {
       render(
         <BrowserRouter>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
         </BrowserRouter>
       );
 
