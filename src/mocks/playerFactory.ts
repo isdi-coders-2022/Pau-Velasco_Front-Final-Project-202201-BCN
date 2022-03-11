0b478b78aa41b74a6dc6e8652b4c08c71aa10d01
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-11 18:50:28 +0100

    add playerFactory function

---
+++ src/mocks/playerFactory.ts
@@ -0,0 +1,17 @@
+import faker from "@faker-js/faker";
+import ObjectID from "bson-objectid";
+import { Factory } from "fishery";
+import { Player } from "../Interfaces/PlayerInterface";
+
+const playerFactory = Factory.define<Player>(() => ({
+  name: faker.name.findName(),
+  number: faker.random.number({ min: 1, max: 99 }),
+  goals: faker.random.number({ min: 1, max: 200 }),
+  assists: faker.random.number({ min: 1, max: 100 }),
+  yellowCards: faker.random.number({ min: 1, max: 20 }),
+  redCards: faker.random.number({ min: 1, max: 20 }),
+  totalMatches: faker.random.number({ min: 1, max: 50 }),
+  position: "Alero",
+  photo: faker.image.imageUrl(),
+  id: ObjectID().toHexString(),
+}));
