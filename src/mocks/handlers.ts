46a38d6c9e25322b6f63caef6acb83a1a22d59de
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-11 12:56:49 +0100

    add handlers file and get request to user/load-user endpoint

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,27 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get(
+    `${process.env.REACT_APP_API_RENDER}user/load-user`,
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json([
+          {
+            username: "Cristiano",
+            teamName: "Man U",
+            password: "afjdsk",
+            players: [],
+            id: "2",
+          },
+          {
+            username: "Messi",
+            teamName: "PSG",
+            password: "afjdsk",
+            players: [],
+            id: "1",
+          },
+        ])
+      )
+  ),
+];
