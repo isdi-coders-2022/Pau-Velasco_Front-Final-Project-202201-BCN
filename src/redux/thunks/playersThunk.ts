f4fcc7deabf0e79d4ba97514ab73745800cbc4c8
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-13 11:29:39 +0100

    add deletePlayerThunk on playersThunks

--- src/redux/thunks/playersThunk.ts
+++ src/redux/thunks/playersThunk.ts
@@ -1,11 +1,14 @@
 import { Dispatch } from "redux";
 import { Player } from "../../Interfaces/PlayerInterface";
-import { loadPlayersAction } from "../actions/actionsCreator";
+import {
+  deletePlayerAction,
+  loadPlayersAction,
+} from "../actions/actionsCreator";
 
-const url: string = `${process.env.REACT_APP_API_RENDER}user/load-user-players`;
+const url: string = process.env.REACT_APP_API_RENDER;
 
 export const loadPlayersThunk = () => async (dispatch: Dispatch) => {
-  const response = await fetch(url, {
+  const response = await fetch(`${url}user/load-user-players`, {
     method: "GET",
     headers: {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhdSIsImlkIjoiNjIyNzE5MDYyYjJhMDIzNzQ1ODYxZDUyIiwiaWF0IjoxNjQ2ODM1ODEzfQ.SjA-pGMQWfRLTPk45sB7YW_eOa1DmK_8CWwS8ANHixg`,
@@ -14,4 +17,17 @@ export const loadPlayersThunk = () => async (dispatch: Dispatch) => {
 
   const players: Player[] = await response.json();
   return dispatch(loadPlayersAction(players));
+};
+
+export const deletePlayerThunk = (id: string) => async (dispatch: Dispatch) => {
+  const response = await fetch(`${url}player/delete-player/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhdSIsImlkIjoiNjIyNzE5MDYyYjJhMDIzNzQ1ODYxZDUyIiwiaWF0IjoxNjQ2ODM1ODEzfQ.SjA-pGMQWfRLTPk45sB7YW_eOa1DmK_8CWwS8ANHixg`,
+    },
+  });
+
+  if (response.ok) {
+    return dispatch(deletePlayerAction(id));
+  }
 };
