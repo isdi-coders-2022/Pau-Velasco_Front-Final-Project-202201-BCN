c0310aefdeb9ffc55940f9781886955b9e432471
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-14 23:36:58 +0100

    add stats inputs on playerForm

--- src/components/PlayerForm/PlayerForm.tsx
+++ src/components/PlayerForm/PlayerForm.tsx
@@ -16,6 +16,32 @@ const PlayerForm = () => {
             <input type="file" id="photo" name="photo" />
           </div>
         </div>
+        <div>
+          <div>
+            <label htmlFor="goals">GOLES</label>
+            <input type="number" id="goals" />
+          </div>
+          <div>
+            <label htmlFor="assists">ASSISTS</label>
+            <input type="number" id="assists" />
+          </div>
+          <div>
+            <label htmlFor="yellowCards">AMARIL</label>
+            <input type="number" id="yellowCards" />
+          </div>
+          <div>
+            <label htmlFor="redCards">ROJAS</label>
+            <input type="number" id="redCards" />
+          </div>
+          <div>
+            <label htmlFor="totalMatches">P. JUGADOS</label>
+            <input type="number" id="totalMatches" />
+          </div>
+          <div>
+            <label htmlFor="position">POSICIÓN</label>
+            <input type="number" id="position" />
+          </div>
+        </div>
       </form>
     </>
   );
