3ddc0e964fb4f84045e93c744d24b581c3b68204
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-15 14:54:08 +0100

    import blankFields from utils folder

--- src/components/PlayerForm/PlayerForm.tsx
+++ src/components/PlayerForm/PlayerForm.tsx
@@ -1,21 +1,14 @@
-import { CreatedPlayer } from "../../Interfaces/PlayerInterface";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createPlayerThunk } from "../../redux/thunks/playersThunk";
+import blankFields from "../../utils/blankFields";
 
-const PlayerForm = () => {
+interface PlayerFormProps {
+  heading: string;
+}
+
+const PlayerForm = ({ heading }: PlayerFormProps): JSX.Element => {
   const dispatch = useDispatch();
-  const blankFields: CreatedPlayer = {
-    name: "",
-    number: "",
-    photo: "",
-    goals: "",
-    assists: "",
-    yellowCards: "",
-    redCards: "",
-    totalMatches: "",
-    position: "",
-  };
   const [formData, setFormData] = useState(blankFields);
   const isInvalid =
     formData.name === "" ||
@@ -45,96 +38,99 @@ const PlayerForm = () => {
 
   return (
     <>
-      <form noValidate autoComplete="off" onSubmit={submitForm}>
-        <div>
+      <div>
+        <h2>{heading}</h2>
+        <form noValidate autoComplete="off" onSubmit={submitForm}>
           <div>
-            <label htmlFor="name">NOMBRE DEL JUGADOR</label>
-            <input
-              type="text"
-              id="name"
-              value={formData.name}
-              onChange={changeData}
-            />
+            <div>
+              <label htmlFor="name">NOMBRE DEL JUGADOR</label>
+              <input
+                type="text"
+                id="name"
+                value={formData.name}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="number">DORSAL</label>
+              <input
+                type="number"
+                id="number"
+                value={formData.number}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="photo"></label>
+              <input
+                type="file"
+                id="photo"
+                onChange={changeFile}
+                accept="image/*"
+              />
+            </div>
           </div>
           <div>
-            <label htmlFor="number">DORSAL</label>
-            <input
-              type="number"
-              id="number"
-              value={formData.number}
-              onChange={changeData}
-            />
+            <div>
+              <label htmlFor="goals">GOLES</label>
+              <input
+                type="number"
+                id="goals"
+                value={formData.goals}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="assists">ASSISTS</label>
+              <input
+                type="number"
+                id="assists"
+                value={formData.assists}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="yellowCards">AMARIL</label>
+              <input
+                type="number"
+                id="yellowCards"
+                value={formData.yellowCards}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="redCards">ROJAS</label>
+              <input
+                type="number"
+                id="redCards"
+                value={formData.redCards}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="totalMatches">P. JUGADOS</label>
+              <input
+                type="number"
+                id="totalMatches"
+                value={formData.totalMatches}
+                onChange={changeData}
+              />
+            </div>
+            <div>
+              <label htmlFor="position">POSICIÓN</label>
+              <input
+                type="text"
+                id="position"
+                value={formData.position}
+                onChange={changeData}
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="photo"></label>
-            <input
-              type="file"
-              id="photo"
-              onChange={changeFile}
-              accept="image/*"
-            />
-          </div>
-        </div>
-        <div>
-          <div>
-            <label htmlFor="goals">GOLES</label>
-            <input
-              type="number"
-              id="goals"
-              value={formData.goals}
-              onChange={changeData}
-            />
-          </div>
-          <div>
-            <label htmlFor="assists">ASSISTS</label>
-            <input
-              type="number"
-              id="assists"
-              value={formData.assists}
-              onChange={changeData}
-            />
-          </div>
-          <div>
-            <label htmlFor="yellowCards">AMARIL</label>
-            <input
-              type="number"
-              id="yellowCards"
-              value={formData.yellowCards}
-              onChange={changeData}
-            />
-          </div>
-          <div>
-            <label htmlFor="redCards">ROJAS</label>
-            <input
-              type="number"
-              id="redCards"
-              value={formData.redCards}
-              onChange={changeData}
-            />
-          </div>
-          <div>
-            <label htmlFor="totalMatches">P. JUGADOS</label>
-            <input
-              type="number"
-              id="totalMatches"
-              value={formData.totalMatches}
-              onChange={changeData}
-            />
-          </div>
-          <div>
-            <label htmlFor="position">POSICIÓN</label>
-            <input
-              type="text"
-              id="position"
-              value={formData.position}
-              onChange={changeData}
-            />
-          </div>
-        </div>
-        <button type="submit" disabled={isInvalid}>
-          CREAR
-        </button>
-      </form>
+          <button type="submit" disabled={isInvalid}>
+            CREAR
+          </button>
+        </form>
+      </div>
     </>
   );
 };
