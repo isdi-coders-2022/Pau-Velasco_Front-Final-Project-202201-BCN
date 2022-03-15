71d60c000b469b54c01d038447d8e52eef1e058a
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-15 09:26:32 +0100

    add createPlayerAction creator

--- src/Interfaces/CreataePlayerActionInterface.ts
+++ src/Interfaces/CreataePlayerActionInterface.ts
@@ -2,5 +2,5 @@ import { Action } from "./ActionInterface";
 import { Player } from "./PlayerInterface";
 
 export interface CreatePlayerAction extends Action {
-  players: Player;
+  player: Player;
 }
