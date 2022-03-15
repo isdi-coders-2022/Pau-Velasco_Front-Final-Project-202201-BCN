61f18ff0a065920efbc39def6e9718b9e4e0ad77
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-15 10:35:47 +0100

    add createPlayerThunk on playersThunk

--- src/redux/reducers/playersReducer.test.ts
+++ src/redux/reducers/playersReducer.test.ts
@@ -7,46 +7,9 @@ import playersReducer from "./playersReducer";
 describe("Given a playersReducer reducer", () => {
   describe("When it receives a list of players in the action with type loadPlayers", () => {
     test("Then it should return the list of players in the action", () => {
-      const expectedPlayers = [
-        {
-          name: "Cristiano",
-          number: 7,
-          goals: 12,
-          assists: 4,
-          yellowCards: 4,
-          redCards: 0,
-          totalMatches: 21,
-          position: "Alero",
-          photo: "photo.jpg",
-          id: "1",
-        },
-        {
-          name: "Messi",
-          number: 7,
-          goals: 12,
-          assists: 4,
-          yellowCards: 4,
-          redCards: 0,
-          totalMatches: 21,
-          position: "Alero",
-          photo: "photo.jpg",
-          id: "1",
-        },
-      ];
-      const initialPlayers = [
-        {
-          name: "",
-          number: 0,
-          goals: 0,
-          assists: 0,
-          yellowCards: 0,
-          redCards: 0,
-          totalMatches: 0,
-          position: "",
-          photo: "",
-          id: "",
-        },
-      ];
+      const initialPlayers = generateRandomPlayers(3);
+      const expectedPlayers = [...initialPlayers, generateRandomPlayer()];
+
       const action = {
         type: "load-players",
         players: expectedPlayers,
