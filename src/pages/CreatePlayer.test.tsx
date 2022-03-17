6c45eba6285140d4510472d8829c621f3e4f05c7
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-17 10:41:52 +0100

    add CreatePlayer render the inputs test

--- src/pages/CreatePlayer.test.tsx
+++ src/pages/CreatePlayer.test.tsx
@@ -22,4 +22,46 @@ describe("Given a CreatePlayer page", () => {
       expect(findTitle).toBeInTheDocument();
     });
   });
+
+  describe("When it's rendered with a title, and nine inputs", () => {
+    test("Then it should a title", () => {
+      const title = "añade un nuevo jugador a tu equipo";
+      const nameInput = /nombre del jugador/i;
+      const numberInput = /dorsal/i;
+      const goalsInput = /goles/i;
+      const assistsInput = /assists/i;
+      const amarillInput = /amaril/i;
+      const rojInput = /rojas/i;
+      const jugadosInput = /p. jugados/i;
+      const posicionInput = /posición/i;
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <CreatePlayer />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const findTitle = screen.getByText(title);
+      const findName = screen.getByLabelText(nameInput);
+      const findNumber = screen.getByLabelText(numberInput);
+      const findGoals = screen.getByLabelText(goalsInput);
+      const findAssists = screen.getByLabelText(assistsInput);
+      const findAmarill = screen.getByLabelText(amarillInput);
+      const findRoj = screen.getByLabelText(rojInput);
+      const findJugados = screen.getByLabelText(jugadosInput);
+      const findPosicion = screen.getByLabelText(posicionInput);
+
+      expect(findTitle).toBeInTheDocument();
+      expect(findName).toBeInTheDocument();
+      expect(findNumber).toBeInTheDocument();
+      expect(findGoals).toBeInTheDocument();
+      expect(findAssists).toBeInTheDocument();
+      expect(findAmarill).toBeInTheDocument();
+      expect(findRoj).toBeInTheDocument();
+      expect(findJugados).toBeInTheDocument();
+      expect(findPosicion).toBeInTheDocument();
+    });
+  });
 });
