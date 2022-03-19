65c4234296fc117b4d0adff0eb64a25a41b7e15d
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-19 18:46:06 +0100

    added alternative route on app.tsx for the NotFoud page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import CreatePlayer from "./pages/CreatePlayer";
 import MainPage from "./pages/MainPage";
+import NotFound from "./pages/NotFound";
 import UpdatePlayer from "./pages/UpdatePlayer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/main-page" element={<MainPage />} />
         <Route path="/create-player" element={<CreatePlayer />} />
         <Route path="/main-page/update-player/:id" element={<UpdatePlayer />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
