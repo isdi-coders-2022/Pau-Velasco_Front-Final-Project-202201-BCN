c322b14250f5eb6b1c3330d348ca063f8501d2af
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-21 15:03:59 +0100

    set token on headers in the api requests

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -2,13 +2,13 @@ import { Dispatch } from "redux";
 import { User } from "../../Interfaces/UserInterface";
 import { loadUserAction } from "../actions/actionsCreator";
 
-const url: string = `${process.env.REACT_APP_API_RENDER}user/load-user`;
+const url: any = process.env.REACT_APP_API_RENDER;
 
 export const loadUserThunk = () => async (dispatch: Dispatch) => {
-  const response = await fetch(url, {
+  const response = await fetch(`${url}user/load-user`, {
     method: "GET",
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhdSIsImlkIjoiNjIyNzE5MDYyYjJhMDIzNzQ1ODYxZDUyIiwiaWF0IjoxNjQ2ODM1ODEzfQ.SjA-pGMQWfRLTPk45sB7YW_eOa1DmK_8CWwS8ANHixg`,
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 
