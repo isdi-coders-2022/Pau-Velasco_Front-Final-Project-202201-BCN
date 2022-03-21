29e909379379b50d8d9b0d4e8bdd93114decfa84
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-21 15:00:15 +0100

    added loginUserFetch on utils folder

---
+++ src/utils/loginUserFetch.ts
@@ -0,0 +1,23 @@
+import { Token } from "../Interfaces/TokenInterface";
+import { LoggedUser } from "../Interfaces/UserInterface";
+
+const url: any = process.env.REACT_APP_API_RENDER;
+
+export const loginUser = async (loginUser: LoggedUser): Promise<any> => {
+  const response = await fetch(`${url}user/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginUser),
+  });
+
+  try {
+    const token: Token = await response.json();
+
+    localStorage.setItem("token", token.token);
+    return token;
+  } catch (error) {
+    return error;
+  }
+};
