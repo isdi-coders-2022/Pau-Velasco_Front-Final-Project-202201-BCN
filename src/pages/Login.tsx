e6352ef16539d69b433aafa7e59e05361a7a5a38
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-21 10:31:26 +0100

    added basic login form on Login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,19 @@
+const Login = () => {
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="">USERNAME</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">PASSWORD</label>
+          <input type="text" />
+        </div>
+        <button>LOGIN</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
