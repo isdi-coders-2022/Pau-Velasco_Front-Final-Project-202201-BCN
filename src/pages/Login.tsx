346a46daa062339c92eed2cfec08992b00eb93ef
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-21 10:45:29 +0100

    added background style to Login page and id's on inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,40 @@
+import styled from "styled-components";
+import GenericButton from "../components/GenericButton/GenericButton";
+
+const LoginContainer = styled.section`
+  background: linear-gradient(193.32deg, #14213d 45.83%, #000000 100%);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & label {
+    color: #fff;
+  }
+
+  & div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
 const Login = () => {
   return (
     <>
-      <form>
-        <div>
-          <label htmlFor="">USERNAME</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">PASSWORD</label>
-          <input type="text" />
-        </div>
-        <button>LOGIN</button>
-      </form>
+      <LoginContainer>
+        <form>
+          <div>
+            <label htmlFor="username">USERNAME</label>
+            <input id="username" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">PASSWORD</label>
+            <input id="password" type="text" />
+          </div>
+          <GenericButton text="LOGIN" disabled={true} />
+        </form>
+      </LoginContainer>
     </>
   );
 };
