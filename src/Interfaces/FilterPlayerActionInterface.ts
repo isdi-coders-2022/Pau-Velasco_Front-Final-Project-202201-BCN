32ec6d856b903635b0fc84db91d2a970ee35289d
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-22 15:26:30 +0100

    modify FilterPlayerActionInterface to has the players

--- src/Interfaces/FilterPlayerActionInterface.ts
+++ src/Interfaces/FilterPlayerActionInterface.ts
@@ -1,5 +1,7 @@
 import { Action } from "./ActionInterface";
+import { Player } from "./PlayerInterface";
 
 export interface FilterPlayerAction extends Action {
   position: string;
+  players: Player[];
 }
