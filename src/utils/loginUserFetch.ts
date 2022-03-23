1f9d16ba21d119f5592fae5244afe93fd42d95dd
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-23 15:01:31 +0100

    change prop name on loginUserFetch

--- src/utils/loginUserFetch.ts
+++ src/utils/loginUserFetch.ts
@@ -3,13 +3,13 @@ import { LoggedUser } from "../Interfaces/UserInterface";
 
 const url: any = process.env.REACT_APP_API_RENDER;
 
-export const loginUser = async (loginUser: LoggedUser): Promise<any> => {
+export const loginUser = async (loggedUser: LoggedUser): Promise<any> => {
   const response = await fetch(`${url}user/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(loginUser),
+    body: JSON.stringify(loggedUser),
   });
 
   const token: Token = await response.json();
