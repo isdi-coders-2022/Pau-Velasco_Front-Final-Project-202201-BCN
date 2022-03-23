9e5a17a8f21763eae04098690d5d0086fc1e1988
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-23 16:19:37 +0100

    added cypress Login test

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -7,15 +7,9 @@ describe("App", function () {
   });
 
   it("renders the app should find h2 with login", function () {
-    cy.get("h2").should("contain", "Log in");
-  });
-
-  it("should find the 1st input and type kevin", function () {
-    cy.get("input").first().type("kevin").should("have.value", "kevin");
-  });
-
-  it("should find the 2md input and type kevin", function () {
-    cy.get("input").last().type("kevin").should("have.value", "kevin");
+    cy.get("input").first().type("pau");
+    cy.get("input").last().type("pau");
+    cy.get("button").click();
   });
 });
 
