539801a7ef193738a1396823071028ab5da9441b
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-23 18:33:48 +0100

    added navigate to detailPage on info icon

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -110,6 +110,9 @@ const MainPage = (): JSX.Element => {
                 dispatch(deletePlayerThunk(player.id));
                 deleteFeedback(player.name);
               }}
+              detailOnClick={() => {
+                navigate(`/main-page/detail-player/${player.id}`);
+              }}
               key={player.id}
             />
           ))}
